0c2faac24290ea338cdf2641c1419a86a9aa7889
santi <santimir.me@gmail.com>
2022-01-09 15:14:54 +0000

    define better interfaces, changed conditional for case statements in readBlockBody, and included the generic Pick to reuse FileHeader and BlockHeader Interfaces

--- src/readBlocks.ts
+++ src/readBlocks.ts
@@ -1,93 +1,117 @@
 /* eslint no-control-regex: 0 */
 import { IOBuffer } from 'iobuffer';
 
-import {
-  btypes,
-  getMeasurementUnits,
-  OverallSpectraDescription,
-  getXListType,
-} from './types';
+import { FileHeader } from './readFileHeader';
+import { btypes, getMeasurementUnits, getXListType } from './types';
 import { getUUId, isCorrupted } from './utilities';
 
 export interface BlockHeader {
+  /** block identity */
   blockType: string;
+  /** block size including header (16B) in bytes */
   blockSize: number;
+  /** unique id for the block.
+   uuid=0 --> only block with this type .
+  */
   uuid: string;
+}
+
+export type BlockBody = DataBlock | XListBlock | []; //temporary type.
+export interface Block {
+  blockHeader: BlockHeader;
+  blockBody: BlockBody;
 }
 export interface XList {
   type: string;
   units: string;
   data: Float32Array;
 }
-export type BlockBody = Float32Array | Uint32Array | XList | []; //temporary type.
-export interface Block {
-  blockHeader: BlockHeader;
-  blockBody: BlockBody;
-}
 
 /**
  * Block Header Parsing - Block's first 16 bytes
  * @param buffer WDF buffer
- * @param offset byte number where to start reading or uses current offset
- * @return blockHeader Block metadata (block type, block size including header, uuid.)
+ * @param [offset] byte number where to start reading | uses current offset
+ * @return blockHeader Block metadata
  */
 export function readBlockHeader(
   buffer: IOBuffer,
   offset?: number,
 ): BlockHeader {
+  /*an offset can be assigned manually if desired*/
   if (offset) buffer.offset = offset;
 
-  /* read properties */
   const blockType: string = btypes(buffer.readUint32());
-  const uuid: string = getUUId(buffer.readBytes(4)); //block have uuid=0 when they are unique in their type.
-  const blockSize = Number(buffer.readBigUint64()); /* in bytes */
+  const uuid: string = getUUId(buffer.readBytes(4));
+  const blockSize = Number(buffer.readBigUint64()); // Bytes
+
   /* assign with appropriate type */
   return { blockType, uuid, blockSize } as BlockHeader;
+}
+
+interface DataBlock {
+  spectra: Uint32Array[];
+}
+interface XListBlock {
+  type: string;
+  units: string;
+  xList: Float32Array;
 }
 
 /**
  * Parses Block body according to the specific block type
  * @param buffer WDF buffer
- * @param blockHeader
+ * @param fileHeader full file header or {nSpectra,nPoints}
+ * @param blockHeader full block header or {blockSize,blockType}
  * @param offset Where the block's body starts, or uses current buffer offset
  * @return array of datapoints and current buffer offset
  */
 export function readBlockBody(
-  /* data in the order in which was collected in */
   buffer: IOBuffer,
-  blockHeader: BlockHeader,
+  fileHeader: FileHeader | Pick<FileHeader, 'nSpectra' | 'nPoints'>,
+  blockHeader: BlockHeader | Pick<BlockHeader, 'blockSize' | 'blockType'>,
   offset?: number,
 ): BlockBody {
+  const { nSpectra, nPoints } = fileHeader;
+  const { blockSize, blockType } = blockHeader;
+
+  /*an offset can be assigned manually if desired*/
   if (offset) buffer.offset = offset;
 
-  const { blockSize, blockType } = blockHeader;
+  /*take some measurements*/
   const headerSize = 16;
   const bodySize = blockSize - headerSize;
-  //get 'typeless arraybuffer' read depending on the array type
-  const wholeBlock: ArrayBuffer = buffer.readBytes(bodySize).buffer;
 
-  if (blockType === 'WDF_BLOCKID_DATA') {
-    /* 32 bit little endian IEEE floating point values.
-  The number of floating point values is equal to the
-  product of the file header items nspectra and npoints
-  */
-    const dataPoints: Float32Array = new Float32Array(wholeBlock);
-    return dataPoints;
-  } else if (blockType === 'WDF_BLOCKID_XLIST') {
-    const first8Bytes = wholeBlock.slice(0, 8);
-    const [type, units] = new Uint32Array(first8Bytes);
+  /* using case a:{} scopes the variables */
+  switch (blockType) {
+    case 'WDF_BLOCKID_DATA': {
+      let spectras32: Uint32Array[] = [];
+      for (let i = 0; i < nSpectra; i++) {
+        const spectra8: Uint8Array = buffer.readBytes(4 * nPoints);
+        spectras32.push(new Uint32Array(spectra8.buffer));
+      }
+      console.log(spectras32.length, spectras32);
+      const data: DataBlock = { spectra: spectras32 };
+      return data;
+    }
 
-    // For the XList block, the number of floats is equal to npoints.
-    const restOfBlock = wholeBlock.slice(8);
-    const data = new Float32Array(restOfBlock);
-    return {
-      type: getXListType(type),
-      units: getMeasurementUnits(units),
-      data,
-    };
-  } //else if('WDF_BLOCKID_YLIST'){}
-  else {
-    return [];
+    case 'WDF_BLOCKID_XLIST': {
+      const first8Bytes = buffer.readBytes(8);
+      const [type, units] = new Uint32Array(first8Bytes.buffer);
+
+      // For the XList block, the number of floats is equal to npoints.
+      const restOfBlock = buffer.readBytes(bodySize - 8);
+      const data: XListBlock = {
+        type: getXListType(type),
+        units: getMeasurementUnits(units),
+        xList: new Float32Array(restOfBlock),
+      };
+      return data;
+    }
+    //case 'WDF_BLOCKID_YLIST':
+
+    default:
+      buffer.readBytes(bodySize);
+      return [];
   }
 }
 
@@ -98,19 +122,19 @@ export function readBlockBody(
  */
 export function readAllBlocks(
   buffer: IOBuffer,
-  overallSpectraDescription: OverallSpectraDescription,
+  fileHeader: FileHeader,
 ): Block[] {
   let blocks: Block[] = [];
   let blockHeaderTypes: string[] = [];
   while (buffer.length > buffer.offset) {
     const blockHeader = readBlockHeader(buffer);
-    const blockBody = readBlockBody(buffer, blockHeader);
+    const blockBody = readBlockBody(buffer, fileHeader, blockHeader);
     blocks.push({ blockHeader, blockBody });
     blockHeaderTypes.push(blockHeader.blockType);
   }
 
   // check if the block headers are complete
-  isCorrupted(blockHeaderTypes, overallSpectraDescription);
+  isCorrupted(blockHeaderTypes, fileHeader.type);
 
   return blocks;
 }
