749107c7af57676b50ab35cf819630b9d0b0bc00
Luc Patiny <luc@patiny.com>
2022-01-18 11:03:05 +0100

    test: add snapshot regression test

--- src/__tests__/parse.test.ts
+++ src/__tests__/parse.test.ts
@@ -7,7 +7,8 @@ describe('parse', () => {
   it('6x6', () => {
     const wdf = readFileSync(join(__dirname, 'data/6x6.wdf'));
     const result = parse(wdf);
-    expect(result).toBeDefined();
+    expect(Object.keys(result.fileHeader)).toHaveLength(30);
+    expect(result).toMatchSnapshot();
     // we have another parser that should give a pretty similar result
   });
 });
